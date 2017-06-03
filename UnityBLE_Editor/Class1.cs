7b59ed1f9d0ec4af52fd0b633a718b23749557f8
sambaiz <godgourd@gmail.com>
2017-06-04 01:03:09 +0900

    use project

---
+++ UnityBLE_Editor/Class1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+#if WINDOWS_UWP
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Enumeration;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+#endif
+
+namespace UnityBLE
+{
+
+    public class DeviceArgs : EventArgs
+    {
+        public string Name { get; set; }
+        public string DeviceID { get; set; }
+
+        public DeviceArgs(string deviceID)
+        {
+            this.DeviceID = deviceID;
+        }
+
+        public DeviceArgs(string deviceID, string name)
+        {
+            this.Name = name;
+            this.DeviceID = deviceID;
+        }
+    }
+
+    public class CharacteristicArgs : EventArgs
+    {
+        public byte[] Value { get; set; }
+        public Exception ex { get; set; }
+
+        public CharacteristicArgs(byte[] value)
+        {
+            this.Value = value;
+        }
+
+        public CharacteristicArgs(Exception e)
+        {
+            this.ex = e;
+        }
+    }
+
+    public sealed class BLE
+    {
+        public event EventHandler<DeviceArgs> DeviceAdded;
+        public event EventHandler<DeviceArgs> DeviceRemoved;
+        public event EventHandler<CharacteristicArgs> CharacteristicReceived;
+
+#if WINDOWS_UWP
+        private DeviceWatcher deviceWatcher;
+#endif
+
+        public void Start()
+        {
+#if WINDOWS_UWP
+            if (deviceWatcher == null) { 
+
+                string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
+
+                deviceWatcher = DeviceInformation.CreateWatcher(
+                                        "(System.Devices.Aep.ProtocolId:=\"{bb7bb05e-5972-42b5-94fc-76eaa7084d49}\")",
+                                        requestedProperties,
+                                        DeviceInformationKind.AssociationEndpoint);
+
+                deviceWatcher.Added += DeviceWatcher_Added;
+                deviceWatcher.Removed += DeviceWatcher_Removed;
+            }
+            if(deviceWatcher.Status != DeviceWatcherStatus.Started) deviceWatcher.Start();
+
+#endif
+        }
+
+        public void Stop()
+        {
+#if WINDOWS_UWP
+            if (deviceWatcher != null && deviceWatcher.Status == DeviceWatcherStatus.Started)
+            {
+                deviceWatcher.Stop();
+            }
+#endif
+        }
+
+        public void Listen(string deviceId, string serviceUUID, string characteristicUUID)
+        {
+#if WINDOWS_UWP
+            listen(deviceId, serviceUUID, characteristicUUID);
+#endif
+        }
+
+#if WINDOWS_UWP
+
+        private void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation deviceInfo)
+        {
+            if (sender == deviceWatcher)
+            {
+                if (deviceInfo.Name != string.Empty)
+                {
+                    DeviceAdded(this, new DeviceArgs(deviceInfo.Id, deviceInfo.Name));
+                }
+            }
+        }
+
+        private void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate deviceInfoUpdate)
+        {
+            if (sender == deviceWatcher)
+            {
+                DeviceRemoved(this, new DeviceArgs(deviceInfoUpdate.Id));
+            }
+        }
+
+        private async void listen(string deviceId, string serviceUUID, string characteristicUUID)
+        {
+#if WINDOWS_UWP
+            try{
+                var device = await BluetoothLEDevice.FromIdAsync(deviceId);
+        
+                var services = await device.GetGattServicesForUuidAsync(new Guid(serviceUUID));
+
+                if (services.Services.Count == 0)
+                {
+                    throw new Exception("no service");
+                }
+
+                var characteristics = await services.Services[0].GetCharacteristicsForUuidAsync(new Guid(characteristicUUID));
+
+                if(characteristics.Characteristics.Count == 0)
+                {
+                    throw new Exception("no characteristic");
+                }
+
+                characteristics.Characteristics[0].ValueChanged += characteristicChanged;
+
+                await characteristics.Characteristics[0].WriteClientCharacteristicConfigurationDescriptorAsync(
+                    GattClientCharacteristicConfigurationDescriptorValue.Notify
+                );
+        
+            }
+            catch(Exception e)
+            {
+                CharacteristicReceived(this, new CharacteristicArgs(e));
+            }
+#endif
+        }
+
+
+        private void characteristicChanged(
+            GattCharacteristic sender,
+            GattValueChangedEventArgs eventArgs
+        )
+        {
+            byte[] data = new byte[eventArgs.CharacteristicValue.Length];
+            Windows.Storage.Streams.DataReader.FromBuffer(eventArgs.CharacteristicValue).ReadBytes(data);
+
+            // Make data a string for now
+            CharacteristicReceived(this, new CharacteristicArgs(data));
+        }
+#endif
+    }
+}
---
+++ Use/MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+namespace Use
+{
+    public sealed partial class MainPage : Page
+    {
+
+        UnityBLE.BLE ble;
+        List<UnityBLE.DeviceArgs> devices;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            ble = new UnityBLE.BLE();
+            devices = new List<UnityBLE.DeviceArgs>();
+
+            ble.DeviceAdded += deviceAddedAsync;
+            ble.DeviceRemoved += deviceRemovedAsync;
+            ble.CharacteristicReceived += characteristicReceivedAsync;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ble.Start();
+        }
+
+        private void deviceList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ble.Stop();
+            ble.Listen(devices[deviceList.SelectedIndex].DeviceID, serviceUUID.Text, characteristicUUID.Text);
+        }
+
+        private async void deviceAddedAsync(object sender, UnityBLE.DeviceArgs args)
+        {
+            if (sender == ble)
+            {
+                devices.Add(args);
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => deviceList.Items.Add(args.Name + " : " + args.DeviceID));
+            }
+        }
+
+        private async void deviceRemovedAsync(object sender, UnityBLE.DeviceArgs args)
+        {
+            if (sender == ble)
+            {
+                devices.Remove(args);
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => deviceList.Items.Remove(args.Name + " : " + args.DeviceID));
+            }
+        }
+
+        private async void characteristicReceivedAsync(object sender, UnityBLE.CharacteristicArgs args)
+        {
+            if (sender == ble && args.ex == null)
+            {
+                await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => receivedData.Text = System.Text.Encoding.UTF8.GetString(args.Value));
+            }
+        }
+    }
+}
